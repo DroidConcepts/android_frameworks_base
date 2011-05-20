4ec42dcb5c34634ef702d9da5c093121a1e96b11
Woodyman <woodyman.dc@gmail.com>
2011-05-20 12:28:09 -0400

    add color code credit: wysie

--- services/java/com/android/server/status/StatusBarPolicy.java
+++ services/java/com/android/server/status/StatusBarPolicy.java
@@ -425,7 +425,7 @@ public class StatusBarPolicy {
                 new com.android.server.status.StorageNotification(context));
 
         // battery
-        mBatteryData = IconData.makeIcon("battery",
+        mBatteryData = IconData.makeIconNumber("battery",
                 null, com.android.internal.R.drawable.stat_sys_battery_unknown, 0, 0);
         mBatteryIcon = service.addIcon(mBatteryData, null);
 
@@ -648,10 +648,21 @@ public class StatusBarPolicy {
     private final void updateBattery(Intent intent) {
         mBatteryData.iconId = intent.getIntExtra("icon-small", 0);
         mBatteryData.iconLevel = intent.getIntExtra("level", 0);
-        mService.updateIcon(mBatteryIcon, mBatteryData, null);
 
         boolean plugged = intent.getIntExtra("plugged", 0) != 0;
         int level = intent.getIntExtra("level", -1);
+
+        //show battery percentage if not plugged in and status is enabled
+        if (plugged || level >= 100 ||
+                Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.BATTERY_PERCENTAGE_STATUS_ICON, 0) == 0) {
+            mBatteryData.number = -1;
+        } else {
+            mBatteryData.number = level;
+        }
+
+        mService.updateIcon(mBatteryIcon, mBatteryData, null);
+
         if (false) {
             Slog.d(TAG, "updateBattery level=" + level
                     + " plugged=" + plugged
